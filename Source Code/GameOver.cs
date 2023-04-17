8c4af1863d20e901887abe5dd47c56dd91e9818d
ClaytonMoniz <118314166+ClaytonMoniz@users.noreply.github.com>
2023-04-16 21:30:25 -0500

    Add files via upload

---
+++ Source Code/GameOver.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shoot_Out_Game
+{
+    public partial class GameOver : Form
+    {
+        public GameOver()
+        {
+            InitializeComponent();
+            lblPickOne.Hide();
+        }
+        
+        private void btnConfirm_Click(object sender, EventArgs e)
+        {
+            //returns to main menu, restarts from level 1, and exits game -- also prevents multiple check boxes from being selected
+            if (chbxMainMenu.Checked == true && chbxRestart.Checked == false && chbxExit.Checked == false)//returns to main menu
+            {
+                StartMenu menu = new StartMenu();
+                menu.Show();
+                this.Close();
+            }
+            else if (chbxRestart.Checked == true && chbxMainMenu.Checked == false && chbxExit.Checked == false)//restarts from level 1
+            {
+                Level1 level1 = new Level1();
+                level1.Show();
+                this.Close();
+            }
+            else if (chbxExit.Checked == true && chbxMainMenu.Checked == false && chbxRestart.Checked == false)//exits game
+            {
+                Application.Exit();
+                
+            }
+            else//messageBox which displays if multiple boxes are selected
+            {
+                lblPickOne.Show();//can't decide if I want to use label of messagebox
+               //MessageBox.Show("Must pick only one option");
+            }
+        }
+    }
+}
