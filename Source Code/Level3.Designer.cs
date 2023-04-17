4b159fb9115a015f4dbdf161e869f0edc81e70a7
ClaytonMoniz <118314166+ClaytonMoniz@users.noreply.github.com>
2023-04-16 20:13:57 -0500

    Add files via upload

---
+++ Source Code/Level3.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+namespace Shoot_Out_Game
+{
+    partial class Level3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.healthBar = new System.Windows.Forms.ProgressBar();
+            this.txtHealth = new System.Windows.Forms.Label();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.txtAmmo = new System.Windows.Forms.Label();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.lblKeyCollected = new System.Windows.Forms.Label();
+            this.key = new System.Windows.Forms.PictureBox();
+            this.door = new System.Windows.Forms.PictureBox();
+            this.Player = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.key)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.door)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // healthBar
+            // 
+            this.healthBar.Location = new System.Drawing.Point(680, 18);
+            this.healthBar.Name = "healthBar";
+            this.healthBar.Size = new System.Drawing.Size(232, 23);
+            this.healthBar.TabIndex = 17;
+            this.healthBar.Value = 100;
+            // 
+            // txtHealth
+            // 
+            this.txtHealth.AutoSize = true;
+            this.txtHealth.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHealth.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txtHealth.Location = new System.Drawing.Point(605, 18);
+            this.txtHealth.Name = "txtHealth";
+            this.txtHealth.Size = new System.Drawing.Size(69, 24);
+            this.txtHealth.TabIndex = 16;
+            this.txtHealth.Text = "Health:";
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txtScore.Location = new System.Drawing.Point(326, 18);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(80, 24);
+            this.txtScore.TabIndex = 15;
+            this.txtScore.Text = "Score: 0";
+            // 
+            // txtAmmo
+            // 
+            this.txtAmmo.AutoSize = true;
+            this.txtAmmo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAmmo.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txtAmmo.Location = new System.Drawing.Point(12, 18);
+            this.txtAmmo.Name = "txtAmmo";
+            this.txtAmmo.Size = new System.Drawing.Size(86, 24);
+            this.txtAmmo.TabIndex = 14;
+            this.txtAmmo.Text = "Ammo: 0";
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.MainTimerEvent);
+            // 
+            // lblKeyCollected
+            // 
+            this.lblKeyCollected.AutoSize = true;
+            this.lblKeyCollected.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKeyCollected.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblKeyCollected.Location = new System.Drawing.Point(12, 42);
+            this.lblKeyCollected.Name = "lblKeyCollected";
+            this.lblKeyCollected.Size = new System.Drawing.Size(126, 24);
+            this.lblKeyCollected.TabIndex = 21;
+            this.lblKeyCollected.Text = "Key Collected";
+            // 
+            // key
+            // 
+            this.key.Image = global::Shoot_Out_Game.Properties.Resources.key;
+            this.key.Location = new System.Drawing.Point(826, 277);
+            this.key.Name = "key";
+            this.key.Size = new System.Drawing.Size(54, 40);
+            this.key.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.key.TabIndex = 20;
+            this.key.TabStop = false;
+            // 
+            // door
+            // 
+            this.door.Image = global::Shoot_Out_Game.Properties.Resources.door;
+            this.door.Location = new System.Drawing.Point(16, 506);
+            this.door.Name = "door";
+            this.door.Size = new System.Drawing.Size(122, 136);
+            this.door.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.door.TabIndex = 19;
+            this.door.TabStop = false;
+            // 
+            // Player
+            // 
+            this.Player.Image = global::Shoot_Out_Game.Properties.Resources.up;
+            this.Player.Location = new System.Drawing.Point(397, 542);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(71, 100);
+            this.Player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.Player.TabIndex = 18;
+            this.Player.TabStop = false;
+            // 
+            // Level3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(924, 661);
+            this.Controls.Add(this.lblKeyCollected);
+            this.Controls.Add(this.healthBar);
+            this.Controls.Add(this.txtHealth);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.txtAmmo);
+            this.Controls.Add(this.key);
+            this.Controls.Add(this.door);
+            this.Controls.Add(this.Player);
+            this.Name = "Level3";
+            this.Text = "Level3";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.key)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.door)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar healthBar;
+        private System.Windows.Forms.Label txtHealth;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Label txtAmmo;
+        private System.Windows.Forms.PictureBox key;
+        private System.Windows.Forms.PictureBox door;
+        private System.Windows.Forms.PictureBox Player;
+        private System.Windows.Forms.Timer GameTimer;
+        private System.Windows.Forms.Label lblKeyCollected;
+    }
+}
---
+++ Source Code/Win.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace Shoot_Out_Game
+{
+    partial class Win
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.gbxYouWin = new System.Windows.Forms.GroupBox();
+            this.chbxExit = new System.Windows.Forms.CheckBox();
+            this.chbxRestart = new System.Windows.Forms.CheckBox();
+            this.chbxMainMenu = new System.Windows.Forms.CheckBox();
+            this.lblPickOne = new System.Windows.Forms.Label();
+            this.btnConfirm = new System.Windows.Forms.Button();
+            this.gbxYouWin.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(155, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "You Win";
+            // 
+            // gbxYouWin
+            // 
+            this.gbxYouWin.Controls.Add(this.chbxExit);
+            this.gbxYouWin.Controls.Add(this.chbxRestart);
+            this.gbxYouWin.Controls.Add(this.chbxMainMenu);
+            this.gbxYouWin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbxYouWin.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.gbxYouWin.Location = new System.Drawing.Point(127, 105);
+            this.gbxYouWin.Name = "gbxYouWin";
+            this.gbxYouWin.Size = new System.Drawing.Size(213, 150);
+            this.gbxYouWin.TabIndex = 1;
+            this.gbxYouWin.TabStop = false;
+            this.gbxYouWin.Text = "What would you like to do";
+            // 
+            // chbxExit
+            // 
+            this.chbxExit.AutoSize = true;
+            this.chbxExit.Location = new System.Drawing.Point(6, 103);
+            this.chbxExit.Name = "chbxExit";
+            this.chbxExit.Size = new System.Drawing.Size(102, 24);
+            this.chbxExit.TabIndex = 2;
+            this.chbxExit.Text = "Exit Game";
+            this.chbxExit.UseVisualStyleBackColor = true;
+            // 
+            // chbxRestart
+            // 
+            this.chbxRestart.AutoSize = true;
+            this.chbxRestart.Location = new System.Drawing.Point(6, 67);
+            this.chbxRestart.Name = "chbxRestart";
+            this.chbxRestart.Size = new System.Drawing.Size(102, 24);
+            this.chbxRestart.TabIndex = 1;
+            this.chbxRestart.Text = "Play Again";
+            this.chbxRestart.UseVisualStyleBackColor = true;
+            // 
+            // chbxMainMenu
+            // 
+            this.chbxMainMenu.AutoSize = true;
+            this.chbxMainMenu.Location = new System.Drawing.Point(6, 32);
+            this.chbxMainMenu.Name = "chbxMainMenu";
+            this.chbxMainMenu.Size = new System.Drawing.Size(177, 24);
+            this.chbxMainMenu.TabIndex = 0;
+            this.chbxMainMenu.Text = "Return to Main Menu";
+            this.chbxMainMenu.UseVisualStyleBackColor = true;
+            // 
+            // lblPickOne
+            // 
+            this.lblPickOne.AutoSize = true;
+            this.lblPickOne.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPickOne.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblPickOne.Location = new System.Drawing.Point(153, 339);
+            this.lblPickOne.Name = "lblPickOne";
+            this.lblPickOne.Size = new System.Drawing.Size(187, 20);
+            this.lblPickOne.TabIndex = 4;
+            this.lblPickOne.Text = "Must pick only one option";
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConfirm.Location = new System.Drawing.Point(167, 279);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(147, 35);
+            this.btnConfirm.TabIndex = 3;
+            this.btnConfirm.Text = "Confirm";
+            this.btnConfirm.UseVisualStyleBackColor = true;
+            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
+            // 
+            // Win
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(480, 391);
+            this.Controls.Add(this.lblPickOne);
+            this.Controls.Add(this.btnConfirm);
+            this.Controls.Add(this.gbxYouWin);
+            this.Controls.Add(this.label1);
+            this.Name = "Win";
+            this.Text = "Win";
+            this.gbxYouWin.ResumeLayout(false);
+            this.gbxYouWin.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox gbxYouWin;
+        private System.Windows.Forms.CheckBox chbxExit;
+        private System.Windows.Forms.CheckBox chbxRestart;
+        private System.Windows.Forms.CheckBox chbxMainMenu;
+        private System.Windows.Forms.Label lblPickOne;
+        private System.Windows.Forms.Button btnConfirm;
+    }
+}
